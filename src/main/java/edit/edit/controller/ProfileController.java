3a7f51066ccb6d113562e1c89af29d08071e93f6
HyeonJin Song <shwj1602@naver.com>
2023-04-27 01:38:24 +0900

    Feat: profile update 추가

--- src/main/java/edit/edit/controller/ProfileController.java
+++ src/main/java/edit/edit/controller/ProfileController.java
@@ -2,8 +2,10 @@ package edit.edit.controller;
 
 import edit.edit.dto.ResponseDto;
 import edit.edit.dto.profile.ProfileRequestDto;
+import edit.edit.security.UserDetailsImpl;
 import edit.edit.service.ProfileService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,18 +15,21 @@ public class ProfileController {
     private final ProfileService profileService;
 
     @PostMapping("/myprofile")
-    public ResponseDto profilePost(@RequestBody ProfileRequestDto profileRequestDto){
-        return profileService.save(profileRequestDto);
+    public ResponseDto profilePost(@RequestBody ProfileRequestDto profileRequestDto,
+                                   @AuthenticationPrincipal UserDetailsImpl userDetails){
+        return profileService.save(profileRequestDto, userDetails.getMember());
     }
 
-    @GetMapping("/myprofile/{id}")
+    @GetMapping("/profile/{id}")
     public ResponseDto getProfile(@PathVariable Long id){
         return profileService.getProfile(id);
     }
 
     @PutMapping("/myprofile/{id}")
-    public ResponseDto profileUpdate(@PathVariable Long id, @RequestBody ProfileRequestDto profileRequestDto){
-        return profileService.update(id, profileRequestDto);
+    public ResponseDto profileUpdate(@PathVariable Long id,
+                                     @RequestBody ProfileRequestDto profileRequestDto,
+                                     @AuthenticationPrincipal UserDetailsImpl userDetails){
+        return profileService.update(id, profileRequestDto, userDetails.getMember());
     }
 
 }
--- src/main/java/edit/edit/service/ProfileService.java
+++ src/main/java/edit/edit/service/ProfileService.java
@@ -3,7 +3,10 @@ package edit.edit.service;
 import edit.edit.dto.ResponseDto;
 import edit.edit.dto.profile.ProfileRequestDto;
 import edit.edit.dto.profile.ProfileResponseDto;
+import edit.edit.entity.Member;
 import edit.edit.entity.Profile;
+import edit.edit.jwt.JwtUtil;
+import edit.edit.repository.MemberRepository;
 import edit.edit.repository.ProfileRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,27 +17,42 @@ import org.springframework.transaction.annotation.Transactional;
 public class ProfileService {
 
     private final ProfileRepository profileRepository;
+    private final MemberRepository memberRepository;
+    private final JwtUtil jwtUtil;
 
     @Transactional
-    public ResponseDto save(ProfileRequestDto profileRequestDto) {
+    public ResponseDto save(ProfileRequestDto profileRequestDto, Member member) {
         Profile profile = profileRequestDto.toEntity();
         profileRepository.save(profile);
 
+        profile.addMember(member);
         return ResponseDto.setSuccess("profile save success", profile);
     }
 
     @Transactional(readOnly = true)
     public ResponseDto getProfile(Long id) {
-        Profile profile = profileRepository.findById(id).orElseThrow();
+        Profile profile = existProfile(id);
         ProfileResponseDto profileResponseDto = new ProfileResponseDto(profile);
         return ResponseDto.setSuccess("getProfile success", profileResponseDto);
     }
 
     @Transactional
-    public ResponseDto update(Long id, ProfileRequestDto profileRequestDto) {
-        Profile profile = profileRepository.findById(id).orElseThrow();
+    public ResponseDto update(Long id, ProfileRequestDto profileRequestDto, Member member) {
+        Profile profile = existProfile(id);
+        isProfileMember(member, profile);
         profile.updateProfile(profileRequestDto);
 
         return ResponseDto.setSuccess("profile update success", profile);
     }
+
+    private Profile existProfile(Long id){
+        return profileRepository.findById(id).orElseThrow();
+    }
+
+    private void isProfileMember(Member member, Profile profile){
+        if(!profile.getMember().getUserId().equals(member.getUserId())){
+            throw new IllegalArgumentException("일치하는 회원이 아닙니다.");
+        }
+    }
+
 }
