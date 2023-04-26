10f5415269fc4d0e58d3d4a866f068f3cf2331f1
sojk401 <sojk401@gmail.com>
2023-04-26 15:52:53 +0900

    프로젝트 생성

---
+++ src/main/java/edit/edit/EditApplication.java
@@ -0,0 +1,13 @@
+package edit.edit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edit/edit/EditApplicationTests.java
@@ -0,0 +1,13 @@
+package edit.edit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
