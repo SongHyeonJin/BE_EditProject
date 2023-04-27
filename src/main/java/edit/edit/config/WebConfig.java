cef9ab53ce546409c8736857b13268d68ce2c366
sojk401 <sojk401@gmail.com>
2023-04-27 12:26:58 +0900

    Chore: 불필요한 메서드 삭제

--- src/main/java/edit/edit/config/WebConfig.java
+++ src/main/java/edit/edit/config/WebConfig.java
@@ -4,8 +4,6 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.validation.Validator;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -24,12 +22,5 @@ public class WebConfig implements WebMvcConfigurer {
         messageSource.setBasename("classpath:/messages/validation");
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
-    }
-
-    @Override
-    public Validator getValidator() {
-        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
-        bean.setValidationMessageSource(validationMessageSource());
-        return bean;
     }
 }
